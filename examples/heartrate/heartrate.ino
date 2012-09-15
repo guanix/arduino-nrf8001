0625721a84e6a24e4a98aa6e7ed318eec4cb6a08
Guan Yang <guan@yang.dk>
2012-09-15 19:27:02 -0400

    Updated heart rate example

--- examples/heartrate/heartrate.ino
+++ examples/heartrate/heartrate.ino
@@ -1,3 +1,6 @@
+// Set Serial Monitor to 115200
+// Currently this will output large amounts of debugging data
+
 #define NRF_DEBUG 1
 #include <SPI.h>
 #include <nRF8001.h>
@@ -8,25 +11,23 @@ float temperatureC;
 uint8_t pipeStatusReceived, dataSent;
 unsigned long lastSent;
 
+void temperatureHandler(float tempC)
+{
+  Serial.println("received temperature");
+  temperatureC = tempC;
+}
+
 void eventHandler(nRFEvent *event)
 {
   Serial.println("event handler");
-  uint8_t handled = 0;
   
-  if (event->event == NRF_COMMANDRESPONSEEVENT) {
-    if (event->msg.commandResponse.opcode == NRF_GETTEMPERATURE_OP) {
-      temperatureC = event->msg.commandResponse.data.temperature / 4.0;
-      handled = 1;
-    }
-  } else if (event->event == NRF_PIPESTATUSEVENT) {
+  if (event->event == NRF_PIPESTATUSEVENT) {
     if (event->msg.pipeStatus.pipesOpen & 1<<5) {
       pipeStatusReceived = 1;
     }
   }
   
-  if (!handled) {
-    nrf->debugEvent(event);
-  }
+  nrf->debugEvent(event);
 }
 
 void setup() {
@@ -37,6 +38,7 @@ void setup() {
   Serial.begin(115200);
   Serial.println("Hello");
   nrf = new nRF8001(5, 6, 7, &eventHandler);
+  nrf->setTemperatureHandler(&temperatureHandler);
   if ((nrf->setup()) == cmdSuccess) {
     Serial.println("SUCCESS");
   } else {
@@ -58,15 +60,14 @@ void setup() {
 }
 
 void loop() {
-  if (nrf->poll(100) == cmdTimeout) {
-    return;
-  }
+  Serial.println("polling");
+  nrf->poll(2000);
   
   if (pipeStatusReceived && (millis() - lastSent) > 1000 && temperatureC > 0.0) {
     Serial.println("ready to send data");
     uint8_t temp[2];
     temp[0] = 0;
-    temp[1] = round(temperatureC) ;
+    temp[1] = round(temperatureC);
     
     nrf->sendData(5, 2, (uint8_t *)&temp);
     lastSent = millis();
