43a5ba4f152b3f0e5e3e45d8b88354e5d717e1f0
Guan Yang <guan@yang.dk>
2014-06-11 15:40:12 -0400

    In/out sample

---
+++ examples/inout/inout.ino
@@ -0,0 +1,56 @@
+#include <SPI.h>
+#include "services.h"
+#include <nRF8001.h>
+
+// This takes up almost 1k of RAM, but it's a hassle to move into PROGMEM
+// (The official Nordic driver for Arduino figured it out)
+// These two data structures are here because they depend on services.h,
+// which we'd rather place here than in the library folder.
+
+hal_aci_data_t setup_msgs[NB_SETUP_MESSAGES] = SETUP_MESSAGES_CONTENT;
+
+// we could template nRF8001 to add the number of pipes, but that causes
+// problems later
+nRF8001 nrf;
+nRFPipe<uint8_t> inChar;          // one byte
+nRFPipe<uint8_t> outChar;  // string up to 10 characters (zero-terminated? hm)
+
+// TODO: endianness
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("nRF8001 test");
+  
+  // Pin numbers for Nordic's Arduino shield that adapts to their dev kit modules
+  nrf.begin(4, 10, 3, NUMBER_OF_PIPES);
+  
+  inChar.begin(nrf, 1);      // 1 is the pipe number
+  outChar.begin(nrf, 2);
+  
+  nrf.setup(NB_SETUP_MESSAGES, setup_msgs);
+  
+  nrf.connect();
+}
+
+void loop() {
+  unsigned long lastNotify = millis();
+  
+  while (nrf.available()) {
+    if (inChar.changed()) {
+      Serial.print("inChar changed, new value: 0x");
+      Serial.println(inChar.read());
+    }
+    
+    if (millis() - lastNotify > 1000) {
+      if (!outChar.open()) {
+        continue;
+      }
+      
+      Serial.println("sending data on outChar");
+      lastNotify = millis();
+      uint8_t time = lastNotify/1000;
+      outChar.send(time);
+    }
+  }
+}
+
